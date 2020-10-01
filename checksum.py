86d83c79d04d4925f398ec38d9be4f3a8f7c164c
sonybelone <sonymekeme@gmail.com>
2020-10-01 18:43:01 +0400

    deuxieme

--- checksum.py
+++ checksum.py
@@ -1,4 +1,27 @@
 #!/usr/bin/python3
+def asciiLetters(value) :
+    nbr = 0
+    nbr2 = 0 
+    for i in value :
+        print("Lettre : ", i," = ",ord(i))
+        nbr += ord(i)
+    print(nbr)
 
+    return nbr
 
+def asciiLetters2(value) :
+    nbr2 = 0
+    for i in value :
+        if nbr2 == 0:
+            nbr2 = ord(i)
+        else :
+            print("nbr2 : ", nbr2 + ord(i))
+            nbr2 += ord(i) + nbr2
+    
+    print(nbr2)
 
+    return nbr2
+
+asciiLetters("belone")
+print("----------------------------------------------")
+asciiLetters2("belone")
